54dc77f803a021a04d64a72deef12eb6864aba1b
alisa <alisa@dxt.cn>
2017-08-09 15:20:59 +0800

    ,

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import './css/index.css';
+import './css/App.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Indexpage from './js/index/index';
+import Main from './js/main/Main';
+let setFontSize = () =>{
+    let html = document.documentElement;
+    let fontSize = html.clientWidth / 16;
+    html.style.fontSize = fontSize + 'px';
+    document.addEventListener('DOMContentLoaded', setFontSize, false);
+}
+setFontSize()
+ReactDOM.render((
+  <Router>
+    <App>
+      <Switch>
+        <Route exact path="/" component={Indexpage} />
+        <Route path="/main" component={Main} />
+      </Switch>
+    </App>
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+  </Router>
+  ),document.getElementById('root'));
---
+++ src/js/index/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Indexpage extends Component {
+  render() {
+    return (
+      <div className="Indexpage">
+        Indexpage
+      </div>
+    );
+  }
+}
+
+export default Indexpage;
